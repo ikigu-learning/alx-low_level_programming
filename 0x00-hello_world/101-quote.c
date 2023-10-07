d5f50691c5f6d841f55f2728b1345842783778a4
George Ikigu <ikigu.geo@gmail.com>
2023-10-07 10:45:59 +0300

    Task 101: Try 0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  *
+  * main - Entry point
+  *
+  * Return: 1 (Error)
+  *
+  */
+
+int main(void)
+{
+	eprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}

30c22c9cf16bb03e825432b36438e42941ea1b8f
George Ikigu <ikigu.geo@gmail.com>
2023-10-09 23:42:11 +0300

    Task 101: Try 0

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n = 48;
+	int m = 49;
+	int p = 50;
+
+	while (n < 56)
+	{
+		if (n < m && m < p)
+		{
+			putchar(n);
+			putchar(m);
+			putchar(p);
+
+			if (n == 55)
+			{
+				putchar('\n');
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+
+		if (p == 57 && m < 57)
+		{
+			p = 48;
+			m++;
+		}
+		else if (p == 57 && m == 57)
+		{
+			p = 48;
+			m = 48;
+			n++;
+		}
+		else
+		{
+			p++;
+		}
+	}
+
+	return (0);
+}

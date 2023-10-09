88c2b23448abaef0aefcdae76c393bfb1bcf06dc
George Ikigu <ikigu.geo@gmail.com>
2023-10-09 23:48:08 +0300

    Task 101: Try 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,11 +4,13 @@
   *
   * Return: Always 0 (Success)
   */
+
 int main(void)
 {
 	int n = 48;
 	int m = 49;
 	int p = 50;
+
 	while (n < 56)
 	{
 		if (n < m && m < p)
@@ -21,27 +23,24 @@ int main(void)
 				putchar('\n');
 				break;
 			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
+		
 		if (p == 57 && m < 57)
 		{
 			p = 48;
 			m++;
 		}
+		
 		else if (p == 57 && m == 57)
 		{
 			p = 48;
 			m = 48;
 			n++;
 		}
-		else
-		{
-			p++;
-		}
+		
+		p++;
 	}
 	return (0);
 }

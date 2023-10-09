7f79ac5822b1d00c5c222829c70c5ff9a77f7ba4
George Ikigu <ikigu.geo@gmail.com>
2023-10-09 11:04:22 +0300

    Task 101: Try 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
   *
-  * main - Entry point
+  * main - Entry point - Writes error message to stderr
   *
   * Return: 1 (Error)
   *
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	eprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

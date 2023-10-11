f3c141687ae1517f378e67c3f6e4a50b70bf1c19
George Ikigu <ikigu.geo@gmail.com>
2023-10-11 12:43:56 +0300

    Task 0: Try 1

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * _puthchar - prints a character to standard output
+  * @c: the character to be printed
+  *
+  * Return: Always 0 (Success)
+  */
+
+int _putchar(int c)
+{
+	printf(c);
+	return 0;
+}
+
+/**
+  * main - Entry point
+  * @c: the argument for the _putchar function
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(c)
+{
+	_putchar(c);
+}

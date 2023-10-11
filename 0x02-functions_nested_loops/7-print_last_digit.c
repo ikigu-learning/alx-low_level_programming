59506d3f1676d5af7352829a148e3a076846b64e
George Ikigu <ikigu.geo@gmail.com>
2023-10-11 15:47:08 +0300

    Task 7: Try 3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "6-abs.c"
 
 /**
   * print_last_digit - prints the last digit of a number
@@ -9,7 +10,8 @@
 
 int print_last_digit(int n)
 {
-	int retval = n % 10;
+	int retval = _abs(n) % 10;
+	
 	_putchar(retval + 48);
 
 	return (retval);

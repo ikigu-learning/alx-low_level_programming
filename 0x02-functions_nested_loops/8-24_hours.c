263ed24db7e84a581d733492c71c9fcbc3fec73d
George Ikigu <ikigu.geo@gmail.com>
2023-10-11 22:11:39 +0300

    Task 8: Try 3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,47 +1,85 @@
 #include "main.h"
 
 /**
-  * jack_bauer - prints every minute of the day of Jack Bauer
-  *
+  * minutes - increments minutes from 00 to 59
   */
 
-void jack_bauer(void)
+void minutes(int m, int mm, int h, int hh)
 {
-	int m = 48;
-	int mm = 48;
-	int h = 48;
-	int hh = 48;
-
-	while (h < 53)
+	while (m < 54)
 	{
 		_putchar(h);
 		_putchar(hh);
-		_putchar(58);
+		_putchar(':');
 		_putchar(m);
 		_putchar(mm);
-		_putchar('\n');
 
-		if (mm < 57)
+		if (mm == 57 && m == 53)
+		{
+			break;
+		}
+		else if (mm < 57)
 		{
 			mm++;
 		}
-		else if (mm == 57 && m < 53)
+		else if (mm == 57 && m < 54)
 		{
 			mm = 48;
-			m++;
+			m++
 		}
-		else if (mm == 57 && m == 53 && hh < 57)
+	}
+}
+
+/**
+  * hours - increments hours from 00 to 20
+  */
+
+void hours(int m, int mm, int h, int hh)
+{
+	while (h < 51)
+	{
+		minutes();
+
+		if (hh == 57 && h == 48)
 		{
-			mm = 48;
-			m = 48;
+			break;
+		}
+		else if (hh < 57)
+		{
 			hh++;
 		}
-		else if (mm == 57 && m == 53 && hh == 57 && h < 51)
+		else if (hh == 57 && h < 51)
 		{
-			mm = 48;
-			m = 48;
 			hh = 48;
 			h++;
 		}
 	}
+}
+
+/**
+  * hourse21_21 - increments hours from 21 to 23
+  */
+
+void hours21_23(void)
+{
+	while (hh < 52)
+	{
+		minutes();
+		hh++;
+	}
+}
+
+/**
+  * jack_bauer - calculates the number of minutes in JB's day
+  */
+void jack_bauer(void)
+{
+	int m = 48;
+	int mm = 48;
+	int h =48:
+	int hh =48;
+
+	hours(m, mm, h, hh);
+	h++;
+	hours21_21(m, mm, h, hh);
 }

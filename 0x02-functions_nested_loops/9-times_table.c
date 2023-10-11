8d9e64bd69b9973060eeb0c7eab0c061857a4f18
George Ikigu <ikigu.geo@gmail.com>
2023-10-11 23:47:51 +0300

    Task 9: Try 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  * times_table - function that creates a 9x9 multiplication table
+  */
+
+void times_table(void)
+{
+	int m = 0;
+	int d - 0;
+	int res;
+
+	while (m < 10)
+	{
+		while (d < 10)
+		{
+			res = m * d;
+			_putchar((res / 10) + '0');
+			_putchar((res % 10) + '0');
+
+			if (d < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n')
+			}
+
+			d++;
+		}
+
+		m++;
+	}
+}
+

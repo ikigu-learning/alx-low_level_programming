2a03e40fc3a7b42713f16466d1d5700720f28d2c
George Ikigu <ikigu.geo@gmail.com>
2023-10-11 15:21:46 +0300

    Task 7: Try 0

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_last_digit - prints the last digit of a number
+  * n: The number whose last digit is to be printed
+  *
+  * Return: The last digit of n
+  */
+
+int print_last_digit(int n)
+{
+	int retval = n % 10;
+	return (retval);
+}

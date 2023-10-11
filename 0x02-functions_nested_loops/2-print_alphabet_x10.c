3302d1849936bef3c8a1b7017b5d2c16ee02b8c8
George Ikigu <ikigu.geo@gmail.com>
2023-10-11 14:23:02 +0300

    Task 2: Try 0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "1-alphabet.c"
+
+/**
+  * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+  *
+  */
+
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		print_alphabet();
+	}
+
+	_putchar('\n');
+}

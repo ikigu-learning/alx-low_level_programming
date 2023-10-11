4fda30b1f6166842888c101fac29d6aedea280f0
George Ikigu <ikigu.geo@gmail.com>
2023-10-11 16:34:30 +0300

    Task 8: Try 1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+  * jack_bauer - prints every minute of the day of Jack Bauer
   *
   */
 
@@ -20,28 +20,28 @@ void jack_bauer(void)
 		_putchar(m);
 		_putchar(mm);
 		_putchar('\n');
-	
-		if (mm == 57)
+
+		if (mm < 57)
+		{
+			mm++;
+		}
+		else if (mm == 57)
 		{
 			mm = 48;
 			m++;
 		}
-		else if (mm == 57 && m == 53 && hh == 57 && h < 51)
-		{
-			mm = 48;
-			m =48;
-			hh = 48;
-			h++;
-		}
 		else if (mm == 57 && m == 53)
 		{
 			mm = 48;
-			m =48;
+			m = 48;
 			hh++;
 		}
-		else
+		else if (mm == 57 && m == 53 && hh == 57 && h < 51)
 		{
-			mm++;
+			mm = 48;
+			m = 48;
+			hh = 48;
+			h++;
 		}
 	}
 }

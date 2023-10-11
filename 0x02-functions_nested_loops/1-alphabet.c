2308b629aa1a7c67ac9f9b678a17b6dabbe831b1
George Ikigu <ikigu.geo@gmail.com>
2023-10-11 13:07:40 +0300

    Task 1: Try 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main (void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+
+	_putchar('\n');
+	return (0);
+}

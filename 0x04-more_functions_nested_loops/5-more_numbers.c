270a1938f4dc09b61bd0f1ca1c757a14af6190f0
George Ikigu <ikigu.geo@gmail.com>
2023-10-12 20:24:57 +0300

    Task 5: Try 0

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  *
+  * more_numbers - prints 0-14 ten times
+  *
+  */
+
+void more_numbers(void)
+{
+	int x = 0;
+	int y = 0;
+
+	while (x < 10)
+	{
+		while(y < 15)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+
+			_putchar((y % 10) + '0');
+		}
+
+		_puthchar('\n');
+	}
+}

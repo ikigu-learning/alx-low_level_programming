36a69176b2615739ffd6ed42b48804dc23538aea
George Ikigu <ikigu.geo@gmail.com>
2023-10-12 23:35:52 +0300

    Task 9: Try 0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+  * fizz_buzz - prints 0 to 100
+  *
+  * Description: If a number is divisible by 3,
+  * this function prints Fizz.
+  * If a number is divisible by 5,
+  * this function prints Buzz.
+  * But if a number is divisible by both 5 and 5,
+  * this function prints FizzBuzz
+  */
+
+void fizz_buzz(void)
+{
+	int x;
+
+	for (x = 1; x < 101; x++)
+	{
+		if (x % 5 == 0 && x % 3 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (x == 100)
+		{
+			printf("%s", "Buzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+}

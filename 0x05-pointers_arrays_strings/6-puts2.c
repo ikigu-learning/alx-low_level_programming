2703464df86e2cecdf923c60ef07faa0b2c45e5c
George Ikigu <ikigu.geo@gmail.com>
2023-10-16 23:22:02 +0300

    Task 6: Try 0

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+  * _strlen - calculates the length of a string
+  * @s: the string to calculate the length of
+  *
+  * Return: Length of string s
+  */
+
+int _strlen(char *s)
+{
+	int chars = 0;
+	char characters = *s;
+
+	while (characters != '\0')
+	{
+		characters = *(s + chars);
+
+		if (characters == '\0')
+		{
+			break;
+		}
+
+		chars++;
+	}
+
+	return (chars);
+}
+
+/**
+  * _puts2 - prints every other character of str to stdout
+  * @str: the string to print
+  *
+  * Return: Nothing
+  */
+
+void _puts2(char *str)
+{
+	int counter = 0;
+	char character;
+	int length = _strlen - 1;
+
+	while (counter < length)
+	{
+		character = *(str + counter);
+
+		if (character == '\0')
+		{
+			break;
+		}
+
+		_putchar(character);
+		counter + 2;
+	}
+
+	_putchar('\n');
+}

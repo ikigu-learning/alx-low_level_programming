dcaddb3f035be8e13ed6f4dd5e81ab13f8b0f064
George Ikigu <ikigu.geo@gmail.com>
2023-10-17 00:36:24 +0300

    Task 8: Try 0

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_array - prints n elements of array a
+  * @a: array whose elements to print
+  * @n: number of elements to print from a
+  *
+  * Return: Nothing
+  */
+
+void print_array(int *a, int n)
+{
+	int counter = 0;
+	int integer_to_print;
+
+	while (n < counter)
+	{
+		integer_to_print = *(a + counter);
+
+		if (counter == (n - 1))
+		{
+			printf("%d", integer_to_print);
+		}
+		else
+		{
+			printf("%d, ", integer_to_print);
+		}
+
+		counter++;
+	}
+}

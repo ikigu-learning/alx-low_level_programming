9531ae34da97b50c710c1aaf0fbfacb6e4186894
George Ikigu <ikigu.geo@gmail.com>
2023-10-16 22:51:29 +0300

    Task 4: Try 0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string to stdout in reverse
+  * @s: the string to print
+  *
+  * Return: Nothing
+  */
+
+void print_rev(char *s)
+{
+	int counter = _strlen(s);
+	char character;
+
+	while (counter > -1)
+	{
+		character = *(str + counter);
+		_putchar(character);
+		counter--;
+	}
+
+	_putchar('\n');
+}

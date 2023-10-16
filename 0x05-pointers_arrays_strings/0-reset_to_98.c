3eef1f5301ec786a677cbf4f4eff98bad9f367b2
George Ikigu <ikigu.geo@gmail.com>
2023-10-16 19:52:09 +0300

    Task 0: Try 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - resets value a pointer points to to 98
+  * @n - value to be reset
+  *
+  * Return: Nothing
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-reset_to_98.c - Is a function that takes a pointer to an int as a parameter and updates the value it points to to 98

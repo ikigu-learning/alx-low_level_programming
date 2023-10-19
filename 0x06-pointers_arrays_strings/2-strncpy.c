810f0eb3ef18e20149d3b7aa1df18e4674766d62
George Ikigu <ikigu.geo@gmail.com>
2023-10-19 13:34:47 +0300

    Task 2: Add null bytes when n is larger than src length

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,31 @@
+#include "main.h"
+
+/**
+  * _strlen - calculates the length of a string
+  * @s: the string to calculate the length of
+  *
+  * Return: Length of string s
+  */
+
+int _strlen(char *s)
+{
+	int chars = 0;
+	char characters = *s;
+
+	while (characters != '\0')
+	{
+		characters = *(s + chars);
+
+		if (characters == '\0')
+		{
+			break;
+		}
+
+		chars++;
+	}
+
+	return (chars);
+}
 /**
   * _strncpy - copies a string from src to dest
   * @dest: where src is being copied to
@@ -13,7 +41,15 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (n > c)
 	{
-		*(dest + c) = *(src + c);
+		if (c > _strlen(src))
+		{
+			*(dest + c) = '\0';
+		}
+		else
+		{
+			*(dest + c) = *(src + c);
+		}
+
 		c++;
 	}
 

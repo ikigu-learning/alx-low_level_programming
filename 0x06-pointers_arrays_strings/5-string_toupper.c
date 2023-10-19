b96e5c24f1779deac72406c48c612fe31dbec411
George Ikigu <ikigu.geo@gmail.com>
2023-10-19 21:54:42 +0300

    Task 5: Try 0

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+/**
+  * string_toupper - changes all lowercase chars of a string to uppercase
+  * @x: the string
+  *
+  * Return: Pointer to the string
+  */
+
+char *string_toupper(char *x)
+{
+	int j = 0;
+
+	while(x[j] != '\0')
+	{
+		if (x[j] > 96 && x[j] < 123)
+		{
+			x[j] = x[j] - 32;
+		}
+
+		j++;
+	}
+
+	return (x);
+}

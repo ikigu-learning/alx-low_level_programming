aa83d1ddb318884c8c941ccca6e509f55a33efd2
George Ikigu <ikigu.geo@gmail.com>
2023-10-23 18:35:51 +0300

    Task 0: Try 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _memset - fills memory with a constant byte
+  * @s: The area of memory to fill
+  * @b: The byte to fill with
+  * @n: number of bytes to fill
+  *
+  * Return: pointer to s
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+
+	return (s);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 char *_strchr(char *s, char c);
 void _strlen(char *s);
+char *_memset(char *s, char b, unsigned int n);

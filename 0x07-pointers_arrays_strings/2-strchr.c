1042b09161591d1b5ed5e8f64acff25b1b69deb7
George Ikigu <ikigu.geo@gmail.com>
2023-10-23 17:22:21 +0300

    Task 2: Try 0

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _strchr - locates a character in a string
+  * @s: The string to find a character in
+  * @c: The character to find
+  *
+  * Return: pointer to first occurence of c
+  */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
+		if (i == _strlen(s)_)
+		{
+			return (NULL);
+		}
+
+		i++;
+	}
+}
---
+++ 0x07-pointers_arrays_strings/_strlen.c
@@ -0,0 +1,18 @@
+/**
+  * _strlen - counts the length of a string
+  *
+  * Return: length of string
+  */
+
+void _strlen(*s)
+{
+	int len = 0`;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
+

33bf0efdd01c79d3b7e077db00dbb91d105e3c99
George Ikigu <ikigu.geo@gmail.com>
2023-10-23 19:07:45 +0300

    Task 1: Try 0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _memcpy - copies a memory area
+  * @src: area to copy from
+  * @dest: area to copy to
+  * @n: number of bytes to copy
+  *
+  * Return: pointer to dest
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		src[i] = dest[i];
+		i++;
+	}
+
+	return (dest);
+}

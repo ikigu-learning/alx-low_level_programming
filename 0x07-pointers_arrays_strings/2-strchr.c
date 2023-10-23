8b1b9c73da2736ac993ff36d23befb910d75a65e
George Ikigu <ikigu.geo@gmail.com>
2023-10-23 19:15:25 +0300

    return 0 not NULL

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#define NULL ((void *)0)
 
 /**
   * _strchr - locates a character in a string
@@ -24,5 +23,5 @@ char *_strchr(char *s, char c)
 		i++;
 	}
 
-	return (NULL);
+	return (0);
 }

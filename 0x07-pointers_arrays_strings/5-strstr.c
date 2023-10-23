ffcc7827aae509e11a36f68272816f75367b6318
George Ikigu <ikigu.geo@gmail.com>
2023-10-23 23:03:49 +0300

    5?

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,4 @@
-#include "main.h"
-
-/**
-  * _strchr - locates a character in a string
-  * @s: The string to find a character in
-  * @c: The character to find
-  *
-  * Return: pointer to first occurence of c if found.
-  * NULL otherwise
-  */
-
-char *_strchr(char *s, char c)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-
-		i++;
-	}
-
-	return (0);
-}
+#include "main.h"}
 
 /**
   * _strstr - locates a substring
@@ -37,5 +11,5 @@ char *_strchr(char *s, char c)
 
 char *_strstr(char *haystack, char *needle)
 {
-	return (_strchr(haystack, needle[0]));
+	return (_strpbrk(haystack, needle));
 }

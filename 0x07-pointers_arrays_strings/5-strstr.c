343391eeaa7e274c5f4de3489bcb41a73feb1ae1
George Ikigu <ikigu.geo@gmail.com>
2023-10-23 22:55:08 +0300

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,33 @@
 #include "main.h"
 
 /**
+  * _strchr - locates a character in a string
+  * @s: The string to find a character in
+  * @c: The character to find
+  *
+  * Return: pointer to first occurence of c if found.
+  * NULL otherwise
+  */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *nil = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
+		i++;
+	}
+i
+	return (nil);
+}
+
+/**
   * _strstr - locates a substring
   * @haystack: Mother string
   * @needle: baby string

f3e6a58e08e17bff315d90e4c17e3a82b7ef513a
George Ikigu <ikigu.geo@gmail.com>
2023-10-23 22:52:33 +0300

    Task 5: Try 0

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _strstr - locates a substring
+  * @haystack: Mother string
+  * @needle: baby string
+  *
+  * Return: pointer to beginnign of substring
+  * or NULL if no substring
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (_strchr(haystack, needle[0]));
+}

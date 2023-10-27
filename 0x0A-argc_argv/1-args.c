9d181faa009ec12fbc32c796ce585f6b09f1095c
George Ikigu <ikigu.geo@gmail.com>
2023-10-27 19:52:38 +0300

    task 1: try 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - prints number of arguments passed to it
+  * @argc: number of arguments
+  * @argv: an array of arguments
+  *
+  * Return: Always 0(Success)
+  */
+
+int main(int argc, char *argv)
+{
+	printf("%d", argc);
+
+	return (0);
+}

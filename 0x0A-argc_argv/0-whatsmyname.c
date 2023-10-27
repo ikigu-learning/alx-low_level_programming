8de6d12db5be3eaf92af1cc2534e60dbd1e0081a
George Ikigu <ikigu.geo@gmail.com>
2023-10-27 19:28:33 +0300

    Task 0: Try 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - entry to program
+  * @argv - array containing arguments passed to program
+  *
+  * Return: Always 0(Success)
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+

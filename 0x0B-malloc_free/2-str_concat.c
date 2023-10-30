051a3a7cb1ed47313e8eaacb4989a11987fb6b4c
George Ikigu <ikigu.geo@gmail.com>
2023-10-30 22:16:41 +0300

    Task 2: Try 0

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,58 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strlen - calculates the length of a string
+  * @s: the string to calculate the length of
+  *
+  * Return: Length of string s
+  */
+
+int _strlen(char *s)
+{
+	int chars = 0;
+	char characters = *s;
+
+	while (characters != '\0')
+	{
+		characters = *(s + chars);
+
+		if (characters == '\0')
+		{
+			break;
+		}
+
+		chars++;
+	}
+
+	return (chars);
+}
+
+/**
+  * _strncat - concatenates strings dest and src
+  * @dest: string to add to
+  * @src: string added to dest
+  * @n: limit of what to take from src
+  *
+  * Return: pointer to resulting string, dest
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int d = _strlen(dest);
+	int z = 0;
+
+	while (n > z && *(src + z) != '\0')
+	{
+		*(dest + d) = *(src + z);
+		z++;
+		d++;
+	}
+
+	*(dest + d) = '\0';
+
+	return (dest);
+}
 
 /**
   * str_concat - concatenates two strings
@@ -7,3 +61,22 @@
   *
   * Return: pointer to s1, NULL on failure
   */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ret;
+	int size;
+
+	size = _strlen(s1) + _strlen(s2) + 1;
+
+	ret = malloc(sizeof(char) * size);
+
+	if (!ret)
+	{
+		return (0);
+	}
+
+	ret = _strncat(s1, s2);
+
+	return (ret);
+}

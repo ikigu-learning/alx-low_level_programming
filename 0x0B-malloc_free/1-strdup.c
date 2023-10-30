fa785dddb3339ca339cf98c4ae635ec1f03e72c8
George Ikigu <ikigu.geo@gmail.com>
2023-10-30 21:49:06 +0300

    Task 0: all functions needed in one file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,62 @@
 #include <stdlib.h>
 
 /**
+  * _strlen - calculates the length of a string
+  * @s: the string to calculate the length of
+  *
+  * Return: Length of string s
+  */
+
+int _strlen(char *s)
+{
+	int chars = 0;
+	char characters = *s;
+
+	while (characters != '\0')
+	{
+		characters = *(s + chars);
+
+		if (characters == '\0')
+		{
+			break;
+		}
+
+		chars++;
+	}
+
+	return (chars);
+}
+/**
+  * _strncpy - copies a string from src to dest
+  * @dest: where src is being copied to
+  * @src: The source string
+  * @n: The no of chars to copy from str
+  *
+  * Return: The pointer to dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c = 0;
+
+	while (n > c)
+	{
+		if (c > _strlen(src))
+		{
+			*(dest + c) = '\0';
+		}
+		else
+		{
+			*(dest + c) = *(src + c);
+		}
+
+		c++;
+	}
+
+	return (dest);
+}
+
+/**
   * _strdup - returns a pointer to a newly allocated space in memory,
   * which contains a copy of the string given as a parameter.
   * @str: the string to write to the new memory allocation

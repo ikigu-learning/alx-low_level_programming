4b0acb3450b483d58d16b5f1b26713cbfd2b1b02
George Ikigu <ikigu.geo@gmail.com>
2023-10-31 03:50:10 +0300

    Task 3: Try 0

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
   * alloc_grid - returns a pointer to a 2D array of ints
@@ -9,4 +10,28 @@
   * or if either height or width is NULL
   */
 
+int **alloc_grid(int width, int height)
+{
+	int i, j, size, **array;
 
+	if (width < 1 || height < 1)
+	{
+		return (0);
+	}
+
+	size = height * width;
+	array = malloc(sizeof(int) * size);
+
+	if (!array)
+		return (0);
+
+	for (i = 0, i < width, i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			array[j][i] = 0;
+		}
+	}
+
+	return (array);
+}

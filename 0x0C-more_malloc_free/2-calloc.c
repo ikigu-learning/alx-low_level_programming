b46ec705bb7bbb7347244924be3a7c17f67be47b
George Ikigu <ikigu.geo@gmail.com>
2023-11-02 12:26:43 +0300

    task 2: casting return ptr to void

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,15 +16,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-		return (0);
+		return (NULL);
 
 	ptr = malloc(size * nmemb);
 
 	if (!ptr)
-		return (0);
+		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
 
-	return (ptr);
+	return ((void *) ptr);
 }

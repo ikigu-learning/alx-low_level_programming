03ef8c83c250e4f3b362be5d58e8a32c396ad183
George Ikigu <ikigu.geo@gmail.com>
2023-11-02 17:36:48 +0300

    callocing

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(size * nmemb);
+	ptr = calloc(nmemb, size);
 
 	if (!ptr)
 		return (NULL);

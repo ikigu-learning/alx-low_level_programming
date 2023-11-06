fe26226efe78253e6409437a368708a77f1222b0
George Ikigu <ikigu.geo@gmail.com>
2023-11-07 02:28:46 +0300

    4-new_dog.c -> first try

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * new_dog - creates a new dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: dog's owner
+  *
+  * Return: pointer to dog
+  *
+  * Description: uses struct dog from dog.h"
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t doggo = malloc(sizeof(dog_t));
+
+	if (!doggo)
+		return (NULL);
+
+	doggo->name = name;
+	doggo->age = age;
+	doggo->owner = owner;
+
+	return (doggo);
+}

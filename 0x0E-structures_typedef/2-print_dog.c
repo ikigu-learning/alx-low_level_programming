e2907f12431ef52f4595e3241c67b27a94f61b2a
George Ikigu <ikigu.geo@gmail.com>
2023-11-07 01:26:55 +0300

    2-print_dog.c: try 0

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,44 @@
+#include <dog.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * print_dog - prints a struct dog
+  * @d: the dog to print
+  *
+  * Return: Nothing
+  */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+		return (NULL);
+
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
+
+	if (d->age != NULL)
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
+
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
+}
+

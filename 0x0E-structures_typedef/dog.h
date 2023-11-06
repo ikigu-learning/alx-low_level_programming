826d881dfcc7b5171091dce8970c0d8a6548a2af
George Ikigu <ikigu.geo@gmail.com>
2023-11-07 00:33:14 +0300

    Documentation for dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+  * struct dog - declares a dog struct
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: dog's owner
+  */
+
 struct dog
 {
 	char *name;

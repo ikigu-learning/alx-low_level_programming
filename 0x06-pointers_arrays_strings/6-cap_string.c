bcd4f5adf35f9894da55cf9eb79e0ca7421fcdaf
George Ikigu <ikigu.geo@gmail.com>
2023-11-07 00:26:40 +0300

    moved dog.h to alx-low_level_programming

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,16 @@
 /**
-  * string_toupper - changes all lowercase chars of a string to uppercase
+  * cap - changes a lowercase to uppercase
   * @x: the string
   *
-  * Return: Pointer to the string
+  * Return: Nothing
   */
 
-char *string_toupper(char *x)
+void cap(char *x)
 {
-	int j = 0;
-
-	if (x[j] > 96 && x[j] < 123)
+	if (*x> 96 && *x < 123)
 	{
-		x[j] = x[j] - 32;
+		*x = *x - 32;
 	}
-
-	return (x);
 }
 
 /**
@@ -30,31 +26,15 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 32 || s[i] == '\t' || s[i] == '(' || s[i] == '{' || s[i] == '\n')
+		if ((s[i - 1]) == '"' || (s[i - 1]) == ' ' || (s[i - 1]) == '\n' || (s[i - 1]) == '\t')
 		{
-			string_toupper(s + (i + 1));
-		}
-		else if (s[i] == '!' || s[i] == '?' || s[i] == '.' || s[i] == ';' || s[i] == '}' || s[i] == ')')
-		{
-			if (s[i + 1] == 32)
-			{
-			string_toupper(s + (i + 2));
-			}
-		}
-		else if (s[i] == '"')
-		{
-			if (s[i - 1] == 32)
-			{
-				string_toupper(s + (i + 1));
-			}
-			else
-			{
-				string_toupper(s + (i + 2));
-			}
+			cap(s[i]);
 		}
 
 		i++;
 	}
+
+	cap(s);
 
 	return (s);
 }

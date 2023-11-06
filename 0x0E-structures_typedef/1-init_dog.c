6567a1e9bfa5d599ba2ab3c989baacf9b6d4ecc6
George Ikigu <ikigu.geo@gmail.com>
2023-11-07 01:11:39 +0300

    something

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,14 +7,17 @@
   * @name: name of dog
   * @age: age of dog
   * @owner: dog's owner
-  * 
+  *
   * Return: Nothing
   */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{	
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
 

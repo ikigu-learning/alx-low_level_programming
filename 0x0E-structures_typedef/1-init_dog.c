9310719089e7aeaf3a0dfe9bb4c7b990f7fd012f
George Ikigu <ikigu.geo@gmail.com>
2023-11-07 00:48:01 +0300

    1-init_dog.c: try 0 with strcpy

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <string.h>
+
+/**
+  * init_dog - initializes a variable of type struct dog
+  * @d: dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: dog's owner
+  * 
+  * Return: Nothing
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = strcpy(name);
+	d->age = age;
+	d->owner = strcpy(owner);
+}
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,3 +18,4 @@ struct dog
 };
 
 #endif
+void init_dog(struct dog *d, char *name, float age, char *owner);

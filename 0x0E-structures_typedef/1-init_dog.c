aa2a3f040b3c771f76207f2e829a8b91fd89d9e1
George Ikigu <ikigu.geo@gmail.com>
2023-11-07 01:10:08 +0300

    task 1, not using strcpy

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,9 +12,9 @@
   */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-	d->name = strcpy(name);
+{	
+	d->name = name;
 	d->age = age;
-	d->owner = strcpy(owner);
+	d->owner = owner;
 }
 

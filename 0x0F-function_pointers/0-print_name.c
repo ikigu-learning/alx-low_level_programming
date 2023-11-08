0c3b23e62c0d4b37e837e37de0c598bf29258dd4
George Ikigu <ikigu.geo@gmail.com>
2023-11-08 20:24:17 +0300

    betty corrections 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,13 @@
 #include "function_pointers.h"
 
+/**
+  * print_name - calls function pointer passed as argument
+  * @f: function to call
+  * @name: name that's printed by calling f
+  *
+  * Return: Nothing
+  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);

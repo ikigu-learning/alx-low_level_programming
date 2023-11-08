27e3eb59bac7fa2711d19b07aa94d4b86c047713
George Ikigu <ikigu.geo@gmail.com>
2023-11-09 01:34:32 +0300

    task 4 function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int answer;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	answer = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	
+	printf("%d\n", answer);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+  * op_add - adds two integers
+  * @a: one of the integers
+  * @b: the other integer
+  *
+  * Return: the sum
+  */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - subtracts two integers
+  * @a: one of the integers
+  * @b: the other integer
+  *
+  * Return: the difference
+  */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - multiplies two integers
+  * @a: one of the integers
+  * @b: the other integer
+  *
+  * Return: the product
+  */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - divides two integers
+  * @a: one of the integers
+  * @b: the other integer
+  *
+  * Return: the result of division
+  */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+  * op_mod - gets remaind of divsion of two integers
+  * @a: one of the integers
+  * @b: the other integer
+  *
+  * Return: the remainder
+  */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

d2309a3b5532726bdb627a3c66dbd4a1e10f9a74
George Ikigu <ikigu.geo@gmail.com>
2023-11-10 12:00:00 +0300

    try task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+  * print_numbers - prints numbers passed as args
+  * @separator: string to print btwn numbers
+  * @n: number of integers passed to function
+  *
+  * Return: nothing
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i, j;
+	int first_arg;
+
+	va_start(args, n);
+
+	first_arg = va_arg(args, unsigned int);
+	j = 0;
+
+	for (i = first_arg; j < n; j++, i = va_arg(args, unsigned int))
+	{
+		printf("%u", i);
+
+		if (separator && j != (n - 1))
+			printf("%s", separator);
+	}
+
+	printf("\n");
+}
+

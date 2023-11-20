c01ee775a1d4939307bb48388ad4ccefdefdd314
George Ikigu <ikigu.geo@gmail.com>
2023-11-20 22:01:08 +0300

    First attempt at task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,27 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list
- * @h: head node of a list
- *
- * Return: the number of nodes in the list
+* print_listint - prints all the elements of a list
+* @h: head node of a list
+*
+* Return: the number of nodes in the list
 */
 
 size_t print_listint(const listint_t *h)
-{}
+{
+	listint_t *ptr;
+	int count;
+
+	ptr = h;
+	count = 0;
+
+	while (ptr)
+	{
+		printf("%d", ptr->n);
+		count++;
+		ptr = ptr->next;
+	}
+
+	return (count);
+}

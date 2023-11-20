8fff83db7abf562a4ced10f3cb1dec1e9cca8144
George Ikigu <ikigu.geo@gmail.com>
2023-11-21 00:25:07 +0300

    returning NULL, task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,18 +2,21 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a listint_t list
- * @head: the head node
- *
- * Return: Nothing
- *
- * Description: This function also sets the head to NULL
+* free_listint2 - frees a listint_t list
+* @head: the head node
+*
+* Return: Nothing
+*
+* Description: This function also sets the head to NULL
 */
 
 void free_listint2(listint_t **head)
 {
 	if (!(*head))
+	{
+		*head = NULL;
 		return;
+	}
 
 	free_listint((*head)->next);
 

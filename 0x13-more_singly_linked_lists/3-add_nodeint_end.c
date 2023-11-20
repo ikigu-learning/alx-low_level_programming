e52836d66e4dbe435ed7075c01dd27be4c9d0010
George Ikigu <ikigu.geo@gmail.com>
2023-11-20 23:26:49 +0300

    first attempt on task 2

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,12 +1,42 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node at end of listint_t list
- * @head: the head node
- * @n: the data to add
- *
- * Return: address of the new element
+* add_nodeint_end - adds a node at end of listint_t list
+* @head: the head node
+* @n: the data to add
+*
+* Return: address of the new element
 */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
-{}
+{
+	/*create and allocate memory for new node*/
+
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+	/*get to the end of the list*/
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!(*head))
+	{
+		*head = new_node;
+	}
+	else
+	{
+		listint_t *ptr = *head;
+
+		while (ptr->next)
+		{
+			ptr = ptr->next;
+		}
+
+		ptr->next = new_node;
+	}
+
+	return (new_node);
+}

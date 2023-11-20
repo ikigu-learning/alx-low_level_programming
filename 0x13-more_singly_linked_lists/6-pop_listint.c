e62fcc462ddcbb91bdd13d30abbd250c3aa91c4f
George Ikigu <ikigu.geo@gmail.com>
2023-11-21 01:18:40 +0300

    first attempt: task 6 -> no malloc

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,24 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes head node of a listint_t list
- * @head: the head node
- *
- * Return: head node's data, or 0 if list is empty
- *
+* pop_listint - deletes head node of a listint_t list
+* @head: the head node
+*
+* Return: head node's data, or 0 if list is empty
+*
 */
 
 int pop_listint(listint_t **head)
-{}
+{
+	listint_t *copy_of_head;
+	int head_data;
+
+	if (!(*head))
+		return (0);
+
+	copy_of_head = (*head)->next;
+	head_data = (*head)->n;
+	*head = copy_of_head;
+
+	return (head_data);
+}

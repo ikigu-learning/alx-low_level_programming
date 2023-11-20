2ad2ef6e0c98b7791fb20c6edbca2cdadda0eeef
George Ikigu <ikigu.geo@gmail.com>
2023-11-20 23:38:17 +0300

    first attempt on task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,21 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint - frees a listint_t list
- * @head: the head node
- *
- * Return: Nothing
+* free_listint - frees a listint_t list
+* @head: the head node
+*
+* Return: Nothing
 */
 
 void free_listint(listint_t *head)
-{}
+{
+	listint_t *ptr = head;
+
+	while (ptr)
+	{
+		free(ptr);
+
+		ptr = ptr->next;
+	}
+}

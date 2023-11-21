63a0e8c7d8da537229b0da2c6cbb97c9ffc3f2d6
George Ikigu <ikigu.geo@gmail.com>
2023-11-21 13:15:53 +0300

    first attempt: task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,4 +10,26 @@
 */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{}
+{
+	listint_t *ptr;
+	unsigned int count;
+
+	ptr = head;
+	count = 0;
+
+	if (head)
+	{
+		while (ptr)
+		{
+			if (count == index)
+			{
+				return (ptr);
+			}
+
+			count++;
+			ptr = ptr->next;
+		}
+	}
+
+	return (NULL);
+}

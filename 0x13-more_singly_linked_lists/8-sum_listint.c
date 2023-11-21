f8d600aca595146e640403b05c55464d022e8240
George Ikigu <ikigu.geo@gmail.com>
2023-11-21 13:19:45 +0300

    fist attempt: task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,4 +8,23 @@
 */
 
 int sum_listint(listint_t *head)
-{}
+{
+	const listint_t *ptr;
+	int sum;
+
+	ptr = head;
+	sum = 0;
+
+	if (ptr)
+	{
+		while (ptr)
+		{
+			sum += ptr->n;
+			ptr = ptr->next;
+		}
+
+		return (sum);
+	}
+
+	return (0);
+}

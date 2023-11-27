d6165b782e923ad21badb002a4326f23c4b3d780
George Ikigu <ikigu.geo@gmail.com>
2023-11-27 22:52:50 +0300

    trying returning 1 if !text_content

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,6 @@
 #include <stddef.h>
 #include <sys/types.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
   * append_text_to_file - adds text to file
@@ -17,18 +16,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, i;
 	size_t bytes_written;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
+
+	if (!text_content)
+		return (1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
 
-	bytes_written = write(fd, text_content, strlen(text_content));
+	i = 0;
+
+	while (text_content[i])
+		i++;
+
+	bytes_written = write(fd, text_content, i);
 
 	if (bytes_written == (unsigned long)-1)
 	{

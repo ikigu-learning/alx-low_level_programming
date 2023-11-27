bab2e39215c4571a9901177f997957bdb03a53f8
George Ikigu <ikigu.geo@gmail.com>
2023-11-27 20:04:57 +0300

    task 0:first attempt

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <sys/types.h>
+
+/**
+  * read_textfile - reads a text file and prints it to the standard output
+  * @filename: the name of the file to read
+  * @letters: the number of letters to read and print
+  *
+  * Return: number of bytes written
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int bytes_read, bytes_written;
+	char buffer[1025];
+	
+	int fd = open(filename, O_RDONLY);
+	
+	if (!filename || fd == -1)
+		return (0);
+
+	bytes_read = read(fd, buffer, sizeof(buffer));
+	close(fd);
+
+	if (bytes_read <= 0)
+		return (0);
+
+	bytes_written = write(STDOUT_FILENO, buffer, letters);
+
+	if (bytes_written == -1)
+		return (0);
+
+	return (letters);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#include <stddef.h>
+#include <sys/types.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(int);
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

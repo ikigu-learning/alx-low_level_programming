18790e00164d250de7ee703adb6d8d0f73d166d3
George Ikigu <ikigu.geo@gmail.com>
2023-11-28 00:12:01 +0300

    refactoring

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,6 +8,50 @@
 #include <string.h>
 
 /**
+ * check_file_close_fail - handles file closing error
+ * @close: pointer to the value returned by close function
+ * @fd: pointer to the value of file descriptor
+ *
+ * Return: Nothing
+*/
+
+void check_file_close_fail(int *close, int *fd)
+{
+	if (*close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *fd);
+		exit(100);
+	}
+}
+
+/**
+ * handle_read_error - handles read errors if any
+ * @filename: the file we're trying to read
+ *
+ * Return: Nothing
+*/
+
+void handle_read_error(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	exit(98);
+}
+
+/**
+ * handle_write_error - handles write errors
+ * @filename: name of file we're trying to write to
+ *
+ * Return: Nothing
+*/
+
+void handle_write_error(char *filename)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(99);
+}
+
+
+/**
  * cp - copies the contents of one file to another
  * @file_from: the file from which to copy
  * @file_to: the file to copy to
@@ -22,19 +66,13 @@ void cp(char *file_from, char *file_to)
 	char buffer[1024];
 
 	if (!file_from)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		handle_read_error(file_from);
 
 	fd_from = open(file_from, O_RDONLY);
 	fd_to = open(file_to, O_RDWR | O_TRUNC | O_CREAT, 0664);
 
 	if (!fd_to)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+		handle_write_error(file_to);
 
 	bytes_read = 1UL;
 
@@ -43,37 +81,19 @@ void cp(char *file_from, char *file_to)
 		bytes_read = read(fd_from, buffer, 1024);
 
 		if (bytes_read == (long)-1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
+			handle_read_error(file_from);
 
 		bytes_written = write(fd_to, buffer, bytes_read);
 
 		if (bytes_written == (long)-1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
+			handle_write_error(file_to);
 	}
 
 	fd_from_closed = close(fd_from);
-
-	if (fd_from_closed == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+	check_file_close_fail(&fd_from_closed, &fd_from);
 
 	fd_to_closed = close(fd_to);
-
-	if (fd_to_closed == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
-	return;
+	check_file_close_fail(&fd_to_closed, &fd_to);
 }
 
 

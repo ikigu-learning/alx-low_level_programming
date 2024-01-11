c7e268dce769b6f891c0d0fd01545b0d55fdea33
George Ikigu <ikigu.geo@gmail.com>
2024-01-11 17:22:43 +0300

    patch: add more header files project will need

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -2,6 +2,9 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dlistint_t - doubly-linked list
  * @n: integer
@@ -14,8 +17,8 @@
 typedef struct dlistint_t
 {
 	int n;
-	dlistint_t *prev;
-	dlistint_t *next;
+	struct dlistint_t *prev;
+	struct dlistint_t *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);

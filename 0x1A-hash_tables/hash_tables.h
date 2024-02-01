2734bb4160d13c7a85a52d92df1467887e887798
George Ikigu <ikigu.geo@gmail.com>
2024-02-01 23:53:59 +0300

    update: _

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -47,5 +47,6 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 void insert(hash_node_t **head_addr, hash_node_t **node);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 
 #endif

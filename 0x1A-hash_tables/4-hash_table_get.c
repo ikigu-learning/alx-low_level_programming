7f086382450b11935f7f5d6aa07e5df95a24f397
George Ikigu <ikigu.geo@gmail.com>
2024-02-01 23:14:25 +0300

    feat: add hash table getter function

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets a value associcated to @key
+ * @key: key in a hash table to get value for
+ * @ht: the hash table
+ *
+ * Return: the pointer to the value
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *ptr;
+
+	unsigned long int index = key_index(key, ht->size);
+	char *value = NULL;
+
+	if (!ht || !key)
+		return (value);
+
+	ptr = ht->array[index];
+
+	while (ptr)
+	{
+		if (strcmp(key, ptr->key) == 0)
+		{
+			value = ptr->value;
+			break;
+		}
+
+		ptr++;
+	}
+
+	return (value);
+}
